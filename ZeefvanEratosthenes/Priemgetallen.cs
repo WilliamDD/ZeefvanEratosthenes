12879230a781171d3db1ec48649f2e6bc8b257ec
William <william@dedooij.eu>
2022-02-11 20:03:45 +0100

    Initial

---
+++ ZeefvanEratosthenes/Priemgetallen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeefvanEratosthenes
+{
+    public class Priemgetallen
+    {
+        public List<double> Zoek(int maximum)
+        {
+            var getallen = new List<double>();
+            var output = new List<double>();
+            for (double i = 2; i <= maximum; i++)
+            {
+                getallen.Add(i);
+            }
+            while (getallen.Any())
+            {
+                var kleinsteGetal = getallen.OrderBy(x => x).FirstOrDefault();
+                output.Add(kleinsteGetal);
+                for (double i = 1; i <= maximum; i++)
+                {
+                    var bla = kleinsteGetal * i;
+                    getallen.Remove(bla);
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ ZeefvanEratosthenes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Methode
+//Maak een gesorteerde lijst van alle getallen van 2 tot een zelf te kiezen maximum.
+var maximum = 10000;
+var priemgetallen = new ZeefvanEratosthenes.Priemgetallen();
+List<double> output = priemgetallen.Zoek(maximum);
+
+Console.WriteLine(String.Join(", ", output));
+Console.WriteLine(output.Count);
